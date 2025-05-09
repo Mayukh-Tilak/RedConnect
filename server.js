24cf48eca188fb7b07b2389965c0b80490750867
AnvitaAn <142152897+AnvitaAnanya@users.noreply.github.com>
2025-05-09 23:23:29 +0530

    Update server.js -> Security concerns

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '1234',
+  password: 'yourpassword',
   database: 'redconnect'
 });
 
